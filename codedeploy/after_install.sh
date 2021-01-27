9c8e96175c2748ae4dc99dc5980bdce1f7a4fb97
Blaž Zdolšek <zdolsekb@gmail.com>
2021-01-27 18:26:48 +0100

    EDIT export NVM_DIR

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ec2-user/app/
-sudo su
 npm install
